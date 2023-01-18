dc4ede3061b5b58de7f8b2a3feca8e213b5b9505
Antoine Gottardi <122371584+antoinegottardi@users.noreply.github.com>
2023-01-18 18:32:12 +0100

    Update Bibliotheque.py

--- daemon/Bibliotheque/Bibliotheque.py
+++ daemon/Bibliotheque/Bibliotheque.py
@@ -105,10 +105,45 @@ def parsejsonexemple():
         # trier les données en fonction de ce qui est recherché 
         
         for i in range(len(nom)):
-            f1=open(f"velo_{nom[i]}.txt","a",encoding="utf-8")
+            f1=open(f"velo_{nom[i]}.txt","a",encoding="utf8")
             f1.write(f"{nom[i]}"+ "\n" )
             f1.write(f"{data['features'][i]['properties']['installati']}" + "\n")
             f1.write(f"{data['features'][i]['properties']['commune']}" + "\n")
             f1.close()
 
-print(parsejsonexemple())
+def placeLibre():
+    """
+    Cette fonction prend la liste des parking de Montpellier, il vous est demander de rentrer un temps de fin 
+    pour pouvoir éffectuer une batterie de test durant toutes cette periode à une fréquence qui vous ai demander 
+    de choisir. Il dis combien il y a de place libre dans chaque parking.
+    bibliothèque obligatoire: request, lxml (fonction etree) et time
+    """
+    tpsFin,tpsIntervall=int(input("choisir temps de fin ")),int(input("choisir un intevalle "))
+    parkings=['Providence-Ovalie', 'PÃ©rols', 'EuromÃ©decine', 'Malbosc', 'HÃ´tel du DÃ©partement', 'Celleneuve', 'Tonnelles', 'Occitanie', 
+    'Fac des Sciences', 'Vert Bois', 'Fac de Lettres', 'Boutonnet', 'Charles Flahault', 'Place Viala', 'PÃ¨re Soulas', 'Aiguelongue', 
+    'Marie Caizergues', 'Jeu de Mail des AbbÃ©s', 'Les Aubes', 'Louis Blanc', 'Emile Combes', 'Corum', 'Beaux-Arts', 'Les Arceaux', 'Foch', 
+    'Plan Cabanes', 'Gambetta', 'Renouvier', 'ClÃ©menceau', 'Halles Castellane', 'Esplanade', 'ComÃ©die', 'Observatoire', 'Rue Jules Ferry',
+    'Saint-Denis', 'Parvis Jules Ferry', 'Nouveau Saint-Roch', 'CitÃ© Mion', 'Voltaire', "Nombre d'Or", 'Antigone Centre', 'MÃ©diathÃ¨que Emile Zola',
+    'HÃ´tel de Ville', 'Port Marianne', 'Richter', 'Jardin de la Lironde', 'Odysseum', 'Garcia Lorca', "PrÃ¨s d'ArÃ¨nes", 'Sabines', 'Rondelet',
+    'Place Albert 1er - St-Charles', 'Montpellier Sud de France', ' Deux Ponts - Gare Saint-Roch', 'Albert 1er - Cathedrale', 
+    'Pont de Lattes - Gare St-Roch', 'Saint-Guilhem - Courreau'] 
+    temps_initial=int(time.time()) 
+    for i in parkings:
+        temps_ini=int(time.time()) 
+        temps=[float(temps_ini+i) for i in range(0,tpsFin+1,tpsIntervall)]
+        tps=int(time.time())
+        f1=open(f"{i}_place.txt","w", encoding='utf8') 
+        maxi=max(temps)
+        while maxi >=tps:
+            if tps in temps: 
+                    f2=open(f"{i}.txt","w", encoding='utf8')
+                    f2.write(response.text)
+                    f2.close()
+                    tree = etree.parse(f"{i}.txt") 
+                    for user in tree.xpath("Free") : 
+                        f1.write(f'Le Parking à vélo:{i} à Nombre de places libres :{user.text}   à un temps de {tps}\n')  
+                    temps.remove(tps)
+                    tps=int(time.time())
+            else:
+                    tps=int(time.time())
+    f1.close()
