be5859f73c9b28b8330926409cb2b05cd8c9280f
Antoine Gottardi <122371584+antoinegottardi@users.noreply.github.com>
2023-01-22 14:10:16 +0100

    Update main.py

--- daemon/main.py
+++ daemon/main.py
@@ -1,5 +1,6 @@
 import subprocess
 import Bibliotheque as b
+from math import sqrt
 d="b"
 projectpath = f"C:\Program Files\gnuplot\{d}in\wgnuplot.exe" 
 subprocess.check_call( ('start',projectpath) , shell=True )
@@ -10,5 +11,7 @@ def moyenne(x:list):
         contenu+=i
     return contenu/len(x)
 
-def ecart_type(x):
-    pass
+def variance(data, ddof=0):
+    moyenn = moyenne(data)
+    return sqrt(sum((x - moyenn) ** 2 for x in data) / (len(data)- ddof))
+print(variance([10,0,10,10,0,0]))
